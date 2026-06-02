4e42172cace772208837f15c5057f9340ab18cf7
Rafael Lamb <rafaelaugustolamb@gmail.com>
2026-06-02 09:15:25 -0300

    AULA 07

---
+++ AULA 07/script.js
@@ -0,0 +1,26 @@
+function exibirnome(){
+
+    let nome = document.querySelector('#nome').value
+
+    alert(nome)
+}
+
+function somar(){
+    let valA = Number(document.querySelector('#valorA').value)
+    let valB = Number(document.querySelector('#valorB').value)
+    alert(valA + valB)
+
+}
+function dividir(){
+    let valA = Number(document.querySelector('#valorA').value)
+    let valB = Number(document.querySelector('#valorB').value)
+    alert(valA / valB)
+
+}
+
+function multiplicar(){
+    let valA = Number(document.querySelector('#valorA').value)
+    let valB = Number(document.querySelector('#valorB').value)
+    alert(valA * valB)
+
+}
